84c5a95197c89dd5e43838a3d7e8b6848e9e8aed
Qvazi <qvazi-s@ya.ru>
2020-05-17 14:30:15 +0500

    Встраивание выражений в JSX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Привет, JSX!</h1>;
+function formatName(user) {
+  const { firstName, lastName } = user;
+  return `${firstName} ${lastName}`;
+}
+
+const user = {
+  firstName: 'Марья',
+  lastName: 'Моревна',
+};
+
+const element = <h1>Здравствуй, {formatName(user)}!</h1>;
 
 ReactDOM.render(element, document.getElementById('root'));

4483269a28c279e4abb98d7854dfc3105d196eb4
Qvazi <qvazi-s@ya.ru>
2020-05-17 15:17:59 +0500

    Извлечение компонентов

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h2>Привет, {props.name}!</h2>;
+function formatDate(date) {
+  const _date = new Date(date);
+  return _date.toLocaleDateString('ru-RU');
+}
+
+function Comment(props) {
+  return (
+    <div className='Comment'>
+      <div className='UserInfo'>
+        <img
+          className='Avatar'
+          src='img/ninja-avatar.png'
+          alt='Ninja'
+          width='100'
+          height='100'
+        />
+        <div className='UserInfo-Name'>Ninja</div>
+      </div>
+      <div className='Comment-text'>I'm best of the best Ninja ever!!!</div>
+      <div className='Comment-date'>{formatDate(new Date())}</div>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <Welcome name='Алиса' />
-      <Welcome name='Базилио' />
-      <Welcome name='Буратино' />
+      <Comment />
     </div>
   );
 }

50e0d4e4e98f4ee67bd5a138713085d0e25c89d3
Qvazi <qvazi-s@ya.ru>
2020-05-17 19:28:44 +0500

    NameForm

--- src/index.js
+++ src/index.js
@@ -5,23 +5,53 @@ import logger from './helpers/logger';
 
 const log = logger('dev');
 
-function Form() {
-  return (
-    <form>
-      <label>
-        Имя:
-        <input type='text' name='name' />
-      </label>
-      <input type='submit' value='Отправить' />
-    </form>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.value);
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Имя:
+          <input
+            value={this.state.value}
+            onChange={this.handleChange}
+            type='text'
+            name='name'
+          />
+        </label>
+        <input type='submit' value='Отправить' />
+      </form>
+    );
+  }
 }
 
 function App() {
   log('App');
   return (
     <React.Fragment>
-      <Form />
+      <NameForm />
     </React.Fragment>
   );
 }

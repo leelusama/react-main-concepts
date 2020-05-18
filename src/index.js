455812e36b134f16c522160665310b563e9c543a
Qvazi <qvazi-s@ya.ru>
2020-05-18 18:32:43 +0500

    Статичный каркас

--- src/index.js
+++ src/index.js
@@ -2,13 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import logger from './helpers/logger';
-import mockPosts from './mock/posts';
-import FormatedDate from './components/FormatedDate/FormatedDate.component';
+import mockProducts from './mock/products';
 
 const log = logger('dev');
 
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <section>
+          <h2>Search form</h2>
+          <form>
+            <p>
+              <label>
+                Search product:
+                <br />
+                <input type='text' placeholder='Ball' />
+              </label>
+            </p>
+            <p>
+              <label>
+                <input type='checkbox' />
+                Only show products in stock
+              </label>
+            </p>
+          </form>
+        </section>
+        <section>
+          <h2>Product List</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th colSpan='2'>Category Name</th>
+              </tr>
+              <tr>
+                <td>Product Name</td>
+                <td>Product Price</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
+    );
+  }
+}
+
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <FilterableProductTable products={mockProducts} />
+    </React.Fragment>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

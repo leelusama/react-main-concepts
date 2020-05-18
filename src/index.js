45d0b9462b04efe9ff6373ef08996cbe1783f806
Qvazi <qvazi-s@ya.ru>
2020-05-18 15:53:45 +0500

    init

--- src/index.js
+++ src/index.js
@@ -7,56 +7,8 @@ import FormatedDate from './components/FormatedDate/FormatedDate.component';
 
 const log = logger('dev');
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      posts: [...mockPosts, ...mockPosts],
-    };
-  }
-
-  render() {
-    log('App');
-
-    const { posts } = this.state;
-
-    return (
-      <React.Fragment>
-        <header>
-          <h1>Это микроблог.</h1>
-        </header>
-        <main>
-          <section>
-            <h2>Посты</h2>
-            <ul>
-              {posts.map((post) => (
-                <li key={post.id}>
-                  <h3>{post.title}</h3>
-                  <p>{post.text}</p>
-                  <p>
-                    Posted: <FormatedDate date={post.createdAt} />
-                  </p>
-                </li>
-              ))}
-            </ul>
-          </section>
-          <section className='post-form'>
-            <h2>Создать пост</h2>
-            <form>
-              <p>
-                <input type='text' placeholder='title' />
-              </p>
-              <p>
-                <textarea rows='5'></textarea>
-              </p>
-            </form>
-          </section>
-        </main>
-        <footer></footer>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return <React.Fragment></React.Fragment>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

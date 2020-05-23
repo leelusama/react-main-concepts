0a8b9d79ac307c90523a2d142ea0c89f47cf3133
leelusama <leelusama@gmail.com>
2020-05-23 14:23:18 +0500

    something like this, so bad

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,41 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        this.setState({ postList: result });
-      })
-      .catch((error) => {});
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const postListResult = await response.json();
+
+      const userIds = [...new Set(postListResult.map((post) => post.userId))];
+
+      const promises = [];
+      userIds.forEach((userId) => {
+        const promise = fetch(
+          `https://jsonplaceholder.typicode.com/users/${userId}`
+        )
+          .then((response) => response.json())
+          .then((result) => result);
+
+        promises.push(promise);
+      });
+
+      const usersResult = await Promise.all(promises);
+
+      const state = {
+        postList: postListResult.map((post) => ({
+          ...post,
+          user: usersResult.find((user) => user.id === post.userId),
+        })),
+      };
+      this.setState(state);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
@@ -27,7 +53,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <h1>Fake Posts</h1>
-        <PostList postList={postList} />
+        {this.state.postList.length > 0 && <PostList postList={postList} />}
       </React.Fragment>
     );
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,44 +1,14 @@
 import React from 'react';
 
-class Post extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: null,
-    };
-  }
-
-  componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/users/${this.props.userId}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        console.log(result, 'result');
-        this.setState({ user: result });
-      })
-      .catch((error) => {});
-  }
-
-  render() {
-    const { title, body } = this.props;
-
-    let element;
-    if (this.state.user) {
-      const { name } = this.state.user;
-      element = (
-        <React.Fragment>
-          <h3>{title}</h3>
-          <p>{body}</p>
-          <p>{`Posted by: ${name}`}</p>
-        </React.Fragment>
-      );
-    } else {
-      element = <span>Loading...</span>;
-    }
-    return element;
-  }
+function Post(props) {
+  const { title, body, user } = props;
+  return (
+    <React.Fragment>
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <p>{`Posted by: ${user.name}`}</p>
+    </React.Fragment>
+  );
 }
 
 export default Post;
